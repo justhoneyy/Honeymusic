2604528c97ca182ed6a0ab5ffed485638210a6c7
Honey <104066113+justhoneyy@users.noreply.github.com>
2026-06-25 11:35:45 +0530

    Initialize models by importing necessary classes

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.user import User, TokenBlocklist, UserPreference
+from app.models.music import Song, Album, Artist, Genre, Lyric, Podcast
+from app.models.playlist import Playlist, PlaylistSong, Like
+from app.models.analytics import ListenHistory, Analytics
