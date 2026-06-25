d0179d487944b585008e1dab4cb17818ce7511bc
Honey <104066113+justhoneyy@users.noreply.github.com>
2026-06-25 14:06:23 +0530

    Create app.js

---
+++ frontend/static/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Honey Music - Static JavaScript Module
+ * This serves as an entry point for future modularization
+ * Currently, all JS is inlined in index.html for single-file SPA
+ * 
+ * This file can be used to progressively enhance the app
+ * by moving modules here as the codebase grows.
+ */
+
+// Placeholder for future modular code
+console.log('🍯 Honey Music loaded');
